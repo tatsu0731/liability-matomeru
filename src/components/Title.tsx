b037be43634bff1ff68638c8bce08abc4fe3454e
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-06-29 12:10:30 +0900

    add: supabaseからデータの取得完了

---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+export default function Title() {
+    return(
+        <h1 className="font-bold text-red-400 text-xl">todoアプリ</h1>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import Title from "@/components/Title";
+import { useEffect, useState } from "react";
+import { getAllTodos } from "../../utils/supabaseFunction";
+
+
+export default function Home() {
+  const [todos, setTodos] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const todos = await getAllTodos();
+      setTodos(todos);
+      console.log(todos);
+    }
+    getTodos();
+  }, []);
+
+  return (
+    <>
+      <Title />
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h1>{todo.title}</h1>
+          <p>{todo.description}</p>
+        </div>
+      ))}
+    </>
+  );
+}
