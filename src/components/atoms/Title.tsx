4deaacd800a873276c3f0b2617920d4b01cb54da
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-07-01 05:02:14 +0900

    fix: タイトルの表示をサイズを中心に修正

--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -1,5 +1,5 @@
 export default function Title() {
     return(
-        <h1 className="font-bold text-white text-xl py-2">負債 matomeru</h1>
+        <h1 className="font-bold text-white text-2xl py-2"><span className="text-xl">負債</span>matomeru</h1>
     )
 }
--- src/components/organisms/Sideber.tsx
+++ src/components/organisms/Sideber.tsx
@@ -3,7 +3,7 @@ import Title from "../atoms/Title";
 
 export default function Sideber() {
     return (
-    <div className=" w-48 h-screen px-4 bg-emerald-400 text-white flex flex-col justify-between items-center border-r-2 border-slate-300">
+    <div className=" w-60 h-screen px-4 bg-emerald-400 text-white flex flex-col justify-between items-center border-r-2 border-slate-300">
         <div>
             <Title />
             {/* <Article /> */}
