08e45179e67deb92b125ba4c5372d0ca42ddc221
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-10-10 23:40:04 +0900

    fix: targetUserが10文字以上だった場合に一部を...に置換するように修正

--- src/components/organisms/Sideber.tsx
+++ src/components/organisms/Sideber.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Title from "../atoms/Title";
-import { getTargetsByUserId, getUserId } from "../../../utils/supabaseFunction";
+import { getUserId } from "../../../utils/supabaseFunction";
 
 import { useEffect, useState } from "react";
 import { supabase } from "../../../utils/supabase";
@@ -15,7 +15,7 @@ export default function Sideber() {
         user_id: string
     }[]>([]);
 
-    const [targetUser, setTargetUser] = useState<string | null>(null);
+    const [targetUser, setTargetUser] = useState<string>("");
     const [error, setError] = useState<boolean>(false);
     const [userId, setUserId] = useState<string | null>(null);
 
@@ -43,19 +43,27 @@ export default function Sideber() {
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
-        try {
-            const { data, error } = await supabase
-            .from('Targets')
-            .insert([
-            { title: targetUser, status: false, user_id: userId },
-            ])
-            .select()
-            if (!error) {
-                setTargets((prevTargets) => [...prevTargets, ...data])
-            }
-        } catch (error) {
-            setError(true)
+        const { data, error } = await supabase
+        .from('Targets')
+        .insert([
+        { title: targetUser, status: false, user_id: userId },
+        ])
+        .select()
+        if (!error) {
+            setTargets((prevTargets) => [...prevTargets, ...data])
+            setTargetUser("")
+            return
         }
+        setError(true)
+    }
+
+    // 10文字以上を...で置換する関数
+    const replaceTitle = (title:string) => {
+        if (title.length > 10) {
+            title = title.slice(0, 10)
+            title = title + "...";
+        }
+        return title
     }
 
     return (
@@ -68,12 +76,12 @@ export default function Sideber() {
                 <h2 className="text-xs font-bold text-slate-200 mt-4 mb-2">■ 債権者一覧</h2>
                 {targets.map((target) => (
                     <Link href={`/${target.id}`} key={target.id}>
-                        <p onClick={() => setTargetId(target.id)}>{target.title}</p>
+                        <p onClick={() => setTargetId(target.id)}>{replaceTitle(target.title)}</p>
                     </Link>
                 ))}
                 <h2 className="text-xs font-bold text-slate-200 mt-4 mb-2">■ 債権者新規作成</h2>
                 <form>
-                    <input type="text" placeholder="債権者名" className="text-black px-1 rounded-md" onChange={(e) => setTargetUser(e.target.value)}/>
+                    <input type="text" value={targetUser} placeholder="債権者名" className="text-black px-1 rounded-md" onChange={(e) => setTargetUser(e.target.value)}/>
                     {error && <p className="text-red-400 text-sm">作成に失敗しました</p>}
                     <button className=" text-emerald-400 py-1 px-4 bg-white rounded-md mt-2 shadow-sm" onClick={handleSubmit}>作成</button>
                 </form>
